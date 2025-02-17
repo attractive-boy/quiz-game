be3aebae1281249b8d5f3ad54b09d8985d85a441
李成 <2414687096@qq.com>
2025-02-17 16:13:36 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,7 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    'linebreak-style': 'off'
   }
 }
